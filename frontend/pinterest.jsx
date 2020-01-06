a7a916f73f93e0e88e7bf2d6a14259b438a398ee
margaret <mmcavenagh@gmail.com>
2020-01-06 15:35:48 -0800

    Add signup, login, and logout functionality

--- frontend/pinterest.jsx
+++ frontend/pinterest.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store';
-import Root from './components/root';
+// import configureStore from './store/store';
+// import Root from './components/root';
+import {signup, login, logout} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    const store = configureStore();
+    // const store = configureStore();
     //FOR TESTING
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
+    window.signup = signup;
+    window.login = login; 
+    window.logout = logout;
     //TEST END
-    ReactDOM.render(<Root store={store} />, root);
+    ReactDOM.render(<h1>Pinterest</h1>, root);
 });
