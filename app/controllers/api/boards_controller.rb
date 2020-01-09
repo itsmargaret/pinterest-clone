64136d0fc879c6725b39d6b22f6d39772fc38a73
margaret <mmcavenagh@gmail.com>
2020-01-08 17:55:55 -0800

    Add boards controller

--- app/controllers/api/boards_controller.rb
+++ app/controllers/api/boards_controller.rb
@@ -1,8 +1,17 @@
 class Api::BoardsController < ApplicationController
+    before_action :require_logged_in 
+
     def index
+        if params[:user_id] 
+            @boards = current_user.boards
+        else 
+            @boards = Board.all 
+        end 
+        render 'api/boards/index'
+            
         #review this w Ronil
-        query = params[:query]
-        @boards = Board.where("name LIKE %?%") 
+        # query = params[:query]
+        # @boards = Board.where("name LIKE %?%") 
     end 
 
     def create
@@ -16,12 +25,26 @@ class Api::BoardsController < ApplicationController
     end 
 
     def show
+        @board = Board.find(params[:id])
+        if @board 
+            render 'api/boards/show'
+        else  
+            json 
     end 
 
     def update
+        @board = Board.find(params[:id])
+        if @board.update(board_params)
+            render 'api/boards/show'
+        else 
+            render json: @board.errors.full_messages, status: 422
+        end 
     end 
 
     def destroy
+        @board = current_user.boards.find(params[:id])
+        @board.destroy
+        render {}
     end 
 
     private 
