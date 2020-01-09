216afc70994c44686a09d5229d36987f3b61e81c
margaret <mmcavenagh@gmail.com>
2020-01-08 21:53:16 -0800

    Updated css and error page

--- frontend/components/error/error.jsx
+++ frontend/components/error/error.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Error = ({ location: { pathname } }) => (
     <section>
         <h1>Sorry, that page doesn't exist.</h1>
+        <a href="https://pinterest-aa.herokuapp.com/">Go Back</a>
     </section>
 );
 
